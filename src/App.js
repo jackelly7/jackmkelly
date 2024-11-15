864a09d141a9ec272beee6e3f389abe910cbc240
Jack Kelly <reactjack7@gmail.com>
2024-11-15 11:46:51 -0700

    switch to react app for more functionality

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Welcome to My Portfolio</h1>
+      </header>
+      <main>
+        <p>This is where I’ll showcase my work.</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
